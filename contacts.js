291f0676ce1523e3c2869de95cceb412fb0ba968
Andrey-Kharchenko <gavryk14@gmail.com>
2024-02-16 19:18:49 +0200

    Fix

--- contacts.js
+++ contacts.js
@@ -16,15 +16,15 @@ const getContactById = async (contactId) => {
 }
 
 const deleteContact = async (contactId) => {
-    const contacts =  await listContacts()
-    const index = contacts.findIndex(el => el.id === contactId)
-    if(index === -1){
-    return null
+    const contacts = await listContacts();
+    const index = contacts.findIndex((el) => el.id === contactId);
+    if (index === -1) {
+        return null;
     }
-    const result = contacts.splice(index, 1)
-    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2))
-    return result
-}
+    const [deletedContact] = contacts.splice(index, 1); 
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+    return deletedContact; 
+};
 
 const addContact = async (data) => {
     const contacts =  await listContacts();
